eadc02271c365a3b28b39e0603b27074a4adf676
y-not-u <voganwong@hotmail.com>
2022-04-09 01:02:52 +0800

    feat: auto updater

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,7 +1,6 @@
 /* eslint import/prefer-default-export: off, import/no-mutable-exports: off */
 import { URL } from 'url';
 import path from 'path';
-import { net } from 'electron';
 
 export let resolveHtmlPath: (htmlFileName: string) => string;
 
@@ -17,39 +16,3 @@ if (process.env.NODE_ENV === 'development') {
     return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
   };
 }
-
-const checkUpdate = async () => {
-  const request = net.request({
-    method: 'GET',
-    protocol: 'https:',
-    hostname: 'tiny-codes.netlify.com',
-    port: 443,
-    path: '/latest.yml',
-  });
-
-  const promise = new Promise<unknown>((resolve, reject) => {
-    request.on('response', (resp) => {
-      const body: Uint8Array[] = [];
-      resp.on('data', (chunk) => {
-        body.push(chunk);
-      });
-
-      resp.on('end', () => {
-        const data = Buffer.concat(body).toString();
-        try {
-          resolve(JSON.parse(data));
-        } catch {
-          resolve(data);
-        }
-      });
-    });
-
-    request.on('error', (err) => {
-      reject(err);
-    });
-  });
-  request.end();
-  return promise;
-};
-
-export { checkUpdate };
